140dbfec955c675e836e7ae2bff04f09a565a619
Alex Pankov <pankov.alex@gmail.com>
2015-12-30 15:28:46 -0800

    Initial Commit

---
+++ mirna_aligner/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  mirna_aligner
+//
+//  Created by Alex P on 12/30/15.
+//  Copyright (c) 2015 Alex Pankov. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
